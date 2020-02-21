b0014f7f9e1d7095d6172413e39c5b6d26a3ef97
ikenna agulobi <ikennaww@yahoo.fr>
2020-02-21 19:28:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language (name , language)
       "Hi, my name is #{name} and I am learning to pragram in #{language}."
    end 
 
-    puts introduction "Josh"
-    puts introduction_with_language "Josh" , "Ruby"
+    p introduction "Josh"
+    p introduction_with_language "Josh" , "Ruby"
 
 
