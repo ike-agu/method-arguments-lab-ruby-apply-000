02410a59abca8408a3b19dd80c6617aa6e429ea7
ikenna agulobi <ikennaww@yahoo.fr>
2020-02-21 19:11:07 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def introduction(name )
+   "Hi, my name is #{name}."
+  end 
+  introduction "Jsh"
+  
+def introduction_with_language (name , language)
+      "Hi, my name is #{name} and I am learning to pragram in #{language}."
+   end 
+
+    puts introduction "Josh"
+    puts introduction_with_language "Josh" , "Ruby"
+
+
